3f56096e05b182d74197c60b6bdef0730b772094
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-06-02 18:43:26 -0600

    Inicio del proyecto de autenticacion con varios proveedores

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
