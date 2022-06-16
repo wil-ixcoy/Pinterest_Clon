7c2903ca354764d30a6a8da8a3d108395bd45d54
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-06-15 22:23:11 -0600

    Cambio en la ubicacion del mensaje inicial

--- index.js
+++ index.js
@@ -37,6 +37,23 @@ app.use(logErrors);
 app.use(errorHandler);
 app.use(boomErrorHandler);
 const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send(`
+    <h1><b>Bienvenidos al clon de Pinterest, para saber como funciona la API y poder probarlo ve a 👇:</b></h1>
+    <h2>
+      <a href="http://localhost:3000/api/docs" style="text-decoration:none;">
+      http://localhost:3000/api/docs
+    </h2>
+    <br>
+    <h3><b>Si quieres conocerme, estas son mis redes👇:</b></h3>
+        <h4><a href="https://www.linkedin.com/in/wiliams-ixcoy-656074229/" style="text-decoration:none;">Mi perfil de linkedIn 💙</a> </h4>
+        <h4><a href="https://twitter.com/wiliamsTI" style="text-decoration:none;">Mi Perfil de Twitter 💚</a> </h4>
+        <h4><a href="https://github.com/wiliamsTI/Clon_Pinterest" style="text-decoration:none;">Link del proyecto en GitHub 🖤</a> </h4>
+        <br>
+    <h3><b>Escribeme :)</b></h3>
+        `);
+
+})
 app.listen(port, () => {
   console.log("listening on port 3000");
 });
--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,6 @@ const AuthRouter = require("./auth.router");
 
 function indexRouter(app) {
   const router = express.Router();
-  app.use("/", (req, res) => {
-    res.send(`
-    <h1><b>Bienvenidos al clon de Pinterest, para saber como funciona la API y poder probarlo ve a 👇:</b></h1>
-    <h2>
-      <a href="https://infinite-meadow-99672.herokuapp.com/api/docs" style="text-decoration:none;">
-        https://infinite-meadow-99672.herokuapp.com/api/docs
-      </a>
-    </h2>
-    <br>
-    <h3><b>Si quieres conocerme, estas son mis redes👇:</b></h3>
-        <h4><a href="https://www.linkedin.com/in/wiliams-ixcoy-656074229/" style="text-decoration:none;">Mi perfil de linkedIn 💙</a> </h4>
-        <h4><a href="https://twitter.com/wiliamsTI" style="text-decoration:none;">Mi Perfil de Twitter 💚</a> </h4>
-        <h4><a href="https://github.com/wiliamsTI/Clon_Pinterest" style="text-decoration:none;">Link del proyecto en GitHub 🖤</a> </h4>
-        <br>
-    <h3><b>Escribeme :)</b></h3>
-        `);
-  });
   app.use("/api", router);
   router.use("/users", userRouter);
   router.use("/images", imageRouter)
